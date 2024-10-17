dd719650320c33cceb30fd88da6cf4900cba9e02
florezandres <andres.f1850@gmail.com>
2024-10-16 21:01:22 -0500

    Http > Https

--- src/main/java/com/example/pawcare/controlador/VeterinarioController.java
+++ src/main/java/com/example/pawcare/controlador/VeterinarioController.java
@@ -20,7 +20,7 @@ import com.example.pawcare.servicio.VeterinarioService;
 
 @RestController
 @RequestMapping("/veterinario")
-@CrossOrigin(origins = "https://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 public class VeterinarioController {
     
     @Autowired
