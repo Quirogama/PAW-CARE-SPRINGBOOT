0d793918b27312c5eae96bc01b47f6dd7d782d9d
florezandres <andres.f1850@gmail.com>
2024-11-05 14:08:01 -0500

    Seguridad v1.4
    Aun no sirve

--- src/main/java/com/example/pawcare/seguridad/CorsConfig.java
+++ src/main/java/com/example/pawcare/seguridad/CorsConfig.java
@@ -1,13 +1,19 @@
 package com.example.pawcare.seguridad;
 
-import org.springframework.context.annotation.Configuration;
+import java.util.Arrays;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CorsConfig {
-
-    /*
-     @Bean
+    @Bean
     public FilterRegistrationBean<CorsFilter> corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 
@@ -18,23 +24,21 @@ public class CorsConfig {
         config.setAllowedHeaders(Arrays.asList(
             HttpHeaders.AUTHORIZATION,
             HttpHeaders.CONTENT_TYPE,
-            HttpHeaders.ACCEPT
+            HttpHeaders.ACCEPT  
         ));
-
-        config.setAllowedMethods(Arrays.asList(
-            HttpMethod.GET.name(),
-            HttpMethod.POST.name(),
-            HttpMethod.PUT.name(),
-            HttpMethod.DELETE.name()
-        ));
+        config.setAllowedMethods(
+            Arrays.asList(
+                HttpMethod.GET.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.PUT.name(),
+                HttpMethod.DELETE.name()
+            )
+        );
 
         source.registerCorsConfiguration("/**", config);
-
         FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
-                
-        bean.setOrder(-102);
-    return bean;
-    }
-     */
 
+        bean.setOrder(-102);
+        return bean;
+    }
 }
--- src/main/java/com/example/pawcare/seguridad/SecurityConfig.java
+++ src/main/java/com/example/pawcare/seguridad/SecurityConfig.java
@@ -1,28 +1,26 @@
 package com.example.pawcare.seguridad;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     
-    /*
-     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .headers(headers -> headers.frameOptions(frame -> frame.disable()))
-                .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(requests -> requests
-                        .requestMatchers("/h2/**").permitAll()
-                        .anyRequest().permitAll()
-                );
-                
-        return http.build();
-
+        @Bean
+        public SecurityFilterChain securityfilterChain(HttpSecurity http) throws Exception {
+                http
+                        .csrf(AbstractHttpConfigurer::disable)
+                        .headers(headers -> headers.frameOptions(frame -> frame.disable()))
+                        .authorizeHttpRequests(requests -> requests
+                                .requestMatchers("/h2/**").permitAll()
+                                .anyRequest().permitAll()
+                        );
+                return http.build();
     }
-     */
-    
 }
