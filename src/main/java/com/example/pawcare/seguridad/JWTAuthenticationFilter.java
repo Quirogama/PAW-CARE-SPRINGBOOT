f344201a0e3846e547317d34ff40578e330e1f62
florezandres <andres.f1850@gmail.com>
2024-11-07 00:50:26 -0500

    Autenticacion creada 2:00:00

--- src/main/java/com/example/pawcare/seguridad/JWTAuthenticationFilter.java
+++ src/main/java/com/example/pawcare/seguridad/JWTAuthenticationFilter.java
@@ -1,5 +1,51 @@
 package com.example.pawcare.seguridad;
 
-public class JWTAuthenticationFilter {
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class JWTAuthenticationFilter extends OncePerRequestFilter{
+
+    @Autowired
+    private JWTGenerator JWTGenerator;
+
+    @Autowired
+    private CustomUserDetailService customUserDetailService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, 
+    HttpServletResponse response,
+    FilterChain filterChain)
+            throws ServletException, IOException {
+        String token = getJWT(request);
+        if(token != null && JWTGenerator.validateToken(token)){
+            String username = JWTGenerator.extractUsername(token);
+            UserDetails userDetails = customUserDetailService.loadUserByUsername(username);
+
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                    userDetails,
+                    null,
+                    userDetails.getAuthorities()
+            );
+
+            
+        }
+    }
+
+    private String getJWT(HttpServletRequest request) {
+        String header = request.getHeader("Authorization");
+        if(header != null && header.startsWith("Bearer ")){
+            return header.replace("Bearer ", "");
+        }
+        return null;
+    }
     
 }
